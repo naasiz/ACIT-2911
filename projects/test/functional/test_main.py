d7a7e5fffeb15ccdb53810658386a2d4eb6cdbe3
Sasi2790 <nguyenthienhuu2005@gmail.com>
2024-05-15 10:43:50 -0700

    New branch

--- projects/test/functional/test_main.py
+++ projects/test/functional/test_main.py
@@ -1,5 +1,7 @@
-# from projects.main import main, add_thread, del_thread, add_comment, login, signup, logout, login_post
+from projects.main import main, add_thread, del_thread, add_comment, login, signup, logout, login_post
 
-# def test_get_main():
-#     response = main()
-#     assert response.status_code == 200
+
+def test_get_main():
+    response = main()
+    assert response.status_code == 200
+
