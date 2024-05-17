6ebf3d4b21475ad53cba22df2e0f9cb825921709
Sasi2790 <nguyenthienhuu2005@gmail.com>
2024-05-17 12:47:18 -0700

    route update

--- projects/main.py
+++ projects/main.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
-from flask_login import login_required, current_user 
+from flask import Blueprint, Flask, render_template, redirect, url_for, request, flash, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_required, current_user, login_user, logout_user
 from db.db import db
 from models.models import User, Thread, Comment, Subheading, Form
 from flask_wtf import FlaskForm
@@ -8,8 +9,9 @@ from wtforms.validators import DataRequired
 
 main = Blueprint('main', __name__)
 
+
 # Main Routes
-# For rendering forums.html
+# For rendering all the threads
 @main.route('/')
 def index():
     statement=db.select(Subheading).order_by(Subheading.id)
@@ -22,34 +24,20 @@ def index():
     except AttributeError:
         return render_template('forums.html', subheadings=results)
 
-# For rendering profile.html
-@main.route('/profile')
+# For rending the currently logged in profile page
+@main.route('/profile', methods=['GET','POST'])
 @login_required
 def profile():
     return render_template('/auth/profile.html',  user=current_user)
 
-# For rendering the thread_detailed.html
+# For rending the thread's comments
 @main.route('/thread_detailed/<int:thread_id>')
 def thread_detailed(thread_id):
     thread = db.get_or_404(Thread, thread_id)
     thread.count = db.session.query(Comment).filter(Comment.thread_id == thread.id).count()
-    try:
-        if int(thread.author.id) == int(current_user.id):
-            print(thread.author.id)
-            print(current_user.id)
-            return render_template("thread_detailed.html", thread = thread, user=current_user, edit = False, own = True)
-        else:
-            return render_template("thread_detailed.html", thread = thread, user=current_user, edit = False, own = False)
-    except:
-        return render_template("thread_detailed.html", thread = thread, user=current_user, edit = False, own = False)
-
-@main.route('/thread_detailed/edit/<int:thread_id>')
-def thread_edit(thread_id):
-    thread = db.get_or_404(Thread, thread_id)
-    thread.count = db.session.query(Comment).filter(Comment.thread_id == thread.id).count()
-    return render_template("thread_detailed.html", thread = thread, user=current_user, edit = True)
-   
-# For rendering the add_thread.html
+    return render_template("thread_detailed.html", thread = thread, user=current_user)
+    
+# For rending the add page
 @main.route('/add')
 def add_page():
     stmt = db.select(Subheading).order_by(Subheading.id)
@@ -81,17 +69,6 @@ def del_thread(thread_id):
     db.session.commit()
     return redirect(url_for('main.index'))
 
-# For editing the thread 
-@main.route("/thread_detailed/editing/<int:thread_id>", methods=["POST"])
-def thread_update(thread_id):
-    thread=db.get_or_404(Thread, thread_id)
-    if request.form["title"] == "":
-        return redirect(url_for('main.thread_detailed', thread_id=thread.id))
-    thread.title=request.form["title"] 
-    thread.content=request.form["content"] 
-    db.session.commit()
-    return redirect(url_for('main.thread_detailed', thread_id=thread.id))
-
 # For adding comments   
 @main.route("/thread_detailed/<int:thread_id>", methods=["POST"])
 def add_comment(thread_id):
@@ -104,6 +81,145 @@ def add_comment(thread_id):
             db.session.add(Comment(thread=thread, content=request.form["content"]))
         db.session.commit()
     return redirect(url_for('main.thread_detailed', thread_id=thread_id))
-        
+            
+# Auth Routes
+@main.route('/login')
+def login():
+    return render_template('/auth/login.html', user=current_user)
+
+@main.route('/signup')
+def signup():
+    return render_template('/auth/signup.html', user=current_user)
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+@main.route('/login', methods=['POST'])
+def login_post():
+    # login code goes here
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    # user = User.query.filter_by(email=email).first()
+    user = db.session.execute(db.select(User).where(User.email == email)).scalar()
+
+    # check if the user actually exists
+    # take the user-supplied password, hash it, and compare it to the hashed password in the database
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+    login_user(user, remember=remember)
+    # if the above check passes, then we know the user has the right credentials
+    return redirect(url_for('main.profile'))
+
+@main.route('/signup', methods=['POST'])
+def signup_post():
+    # code to validate and add user to database goes here
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    # user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+    user = db.session.execute(db.select(User).where(User.email == email)).scalar()
+    if user: # if a user is found, we want to redirect back to signup page so user can try again
+        flash("Email address already exists")
+        return redirect(url_for('auth.signup'))
+
+    # create a new user with the form data. Hash the password so the plaintext version isn't saved.
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='pbkdf2:sha256'))
+
+    # add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('auth.login'))
+
+@main.route('/homepage')
+def toHome():
+    return render_template('homepage-file/homepage.html')
+
+@main.route('/posts')
+def posts():
+    threads = db.session.query(Thread).order_by(Thread.id).all()
+    for thread in threads:
+        thread.count = db.session.query(Comment).filter(Comment.thread_id == thread.id).count()
+    user = current_user if current_user.is_authenticated else None  # Assuming you're using Flask-Login
+    return render_template('homepage-file/posts.html', threads=threads, user=user)
+
+@main.route('/add_comment/<int:thread_id>', methods=["POST"])
+def add_posts(thread_id):
+    content = request.form["content"]
+    if content:
+        try:
+            user = db.get_or_404(User, current_user.id)
+            thread = db.session.query(Thread).get(thread_id)
+            db.session.add(Comment(author=user, thread=thread, content=content))
+        except:
+            thread = db.session.query(Thread).get(thread_id)
+            db.session.add(Comment(thread=thread, content=content))
+        db.session.commit()
+    return redirect(url_for('main.posts'))
 
 
+@main.route('/upvote', methods=['POST'])
+def upvote():
+    thread_id = request.form.get('thread_id')
+    # Update the upvote count in the database for the specified thread_id
+    # Return the updated count
+    return jsonify({'upvotes': 5})
+
+@main.route('/downvote', methods=['POST'])
+def downvote():
+    thread_id = request.form.get('thread_id')
+    # Update the downvote count in the database for the specified thread_id
+    # Return the updated count
+    return jsonify({'downvotes': 5})
+
+# Update database record
+
+@main.route('/update<int:id>', methods=['GET', 'POST'])
+@login_required
+def update(id):
+    form = Form()
+    name_to_update = db.get_or_404(User, id)
+    if request.method == "POST":
+        # print(form)
+        # print(form.name)
+        # print(form.email)
+        print(request.form['name'])
+        print(request.form['email'])
+        name_to_update.name = request.form['name']
+        name_to_update.email = request.form['email']
+        try:
+            db.session.commit()
+            # flash("User Updated Successfully")
+            # return render_template("update.html", form=form, name_to_update=name_to_update, user=current_user)
+            return redirect(url_for('main.profile'))        
+        except:
+            db.session.commit()
+            # flash("Error, there are problems, try again")
+            return render_template("update.html", form=form, name_to_update=name_to_update,id=id, user=current_user)
+    else:
+        return render_template("update.html", form=form, name_to_update=name_to_update,id=id, user=current_user)
+
+
+
+
+
+# @main.route('/update', methods=['GET', 'POST'])
+# def update():
+#     name = None
+#     form = Form()
+#     # Validate form
+#     if form.validate_on_submit():
+#         name = form.name.data
+#         form.name.data = ""
+#     return render_template("update.html", name = name, form = form, user = current__user)
+if __name__ == "__main__":
+    main.run(debug=True)
+
+# if __name__ == "__main__":
+#     main.run(host='0.0.0.0', port=4000)
