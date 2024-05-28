c148602cf3d2946e0a9bed67b0d5fcb9b17bf431
Ray Liu <aliu150@my.bcit.ca>
2024-05-27 21:01:50 -0700

    add downvote and make intro animation play only once

--- projects/models/models.py
+++ projects/models/models.py
@@ -1,4 +1,4 @@
-from db.db import db 
+from db.db import db
 from sqlalchemy import Integer, String, ForeignKey, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from flask_login import UserMixin
@@ -7,8 +7,9 @@ from wtforms import StringField, SubmitField, TextAreaField, DateField
 from wtforms.validators import DataRequired
 from flask_wtf.file import FileField
 
+
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) 
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
@@ -16,15 +17,17 @@ class User(UserMixin, db.Model):
     date_of_birth = db.Column(db.Date)
     threads = relationship("Thread", cascade="all, delete")
     comments = relationship("Comment", cascade="all, delete")
-    profile_pic = db.Column(db.String(), nullable = True)
-    
+    profile_pic = db.Column(db.String(), nullable=True)
+
+
 class Subheading(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     threads = relationship("Thread", cascade="all, delete")
-    title = db.Column(db.String(1000), unique=True)    
+    title = db.Column(db.String(1000), unique=True)
+
 
 class Thread(db.Model):
-    id = db.Column(db.Integer, primary_key=True) 
+    id = db.Column(db.Integer, primary_key=True)
     user_id = mapped_column(Integer, ForeignKey(User.id), nullable=True)
     subheading_id = mapped_column(Integer, ForeignKey(Subheading.id), nullable=True)
     title = db.Column(db.String(100), unique=True)
@@ -33,9 +36,10 @@ class Thread(db.Model):
     comments = relationship("Comment", cascade="all, delete")
     subheading = relationship("Subheading", back_populates="threads")
     content = db.Column(db.String(1000))
-    
+
+
 class Comment(db.Model):
-    id = db.Column(db.Integer, primary_key=True) 
+    id = db.Column(db.Integer, primary_key=True)
     user_id = mapped_column(Integer, ForeignKey(User.id), nullable=True)
     thread_id = mapped_column(Integer, ForeignKey(Thread.id), nullable=False)
     author = relationship("User", back_populates="comments")
@@ -43,8 +47,11 @@ class Comment(db.Model):
     date = db.Column(DateTime(timezone=True), server_default=func.now())
     content = db.Column(db.String(1000))
     # For replies
-    parent_id = mapped_column(db.Integer, db.ForeignKey('comment.id'))
-    replies = db.relationship('Comment', backref=db.backref('parent', remote_side=[id]), lazy='dynamic')  # new field for replies
+    parent_id = mapped_column(db.Integer, db.ForeignKey("comment.id"))
+    replies = db.relationship(
+        "Comment", backref=db.backref("parent", remote_side=[id]), lazy="dynamic"
+    )  # new field for replies
+
 
 class User_Thread_Upvotes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -53,20 +60,27 @@ class User_Thread_Upvotes(db.Model):
     user = relationship("User")
     thread = relationship("Thread")
 
+
+class User_Thread_Downvotes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = mapped_column(Integer, ForeignKey(User.id), nullable=True)
+    thread_id = mapped_column(Integer, ForeignKey(Thread.id), nullable=True)
+    user = relationship("User")
+    thread = relationship("Thread")
+
+
 # Create a class Form
 class Form(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
     description = TextAreaField("Description", validators=[DataRequired()])
     date_of_birth = DateField("Date of Birth")
-    profile_pic = FileField('Profile Pic')
+    profile_pic = FileField("Profile Pic")
     submit = SubmitField("Submit")
+
 
 # class for config the app.config for both app.py and main.py
 class Config(object):
-    SECRET_KEY = 'secret-key-goes-here'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
-    UPLOAD_FOLDER = './static/images'
-
-    
-
+    SECRET_KEY = "secret-key-goes-here"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite"
+    UPLOAD_FOLDER = "./static/images"
